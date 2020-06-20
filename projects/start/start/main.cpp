83c6d8a2b6a00c82de5476b51d9f80b9c37001bc
rurumimic <unhyop@gmail.com>
2020-06-20 23:30:13 +0900

    OpenCV MacOS install

---
+++ projects/start/start/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  start
+//
+//  Created by Dohyun Kim on 20/06/2020.
+//  Copyright © 2020 rurumimic. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, const char * argv[]) {
+    
+    cout << "OpenCV version: " << CV_VERSION << endl;
+    
+    return 0;
+}
